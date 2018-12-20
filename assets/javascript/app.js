a36d8d75a0dce39f449d0a11c6909890ae8308cd
Jason Kim <jasonkim4201@gmail.com>
2018-12-20 15:43:29 -0500

    added a working timer for the trivia game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,26 +2,41 @@
 //Add button to start the game
 //On clicking start button, a timer should start. setTimeout(functionToRun(), time in milliseconds)
 // Add a timer on screen reflecting the time left.  
+//setinterval if time=0 game over
 $(document).ready(function() {
   console.log("ready");
 });
 
 //Total time
-var timeLeft = 120;
-
+var timeLeft = 90;
+var intervalId;
 
 $("#startButton").click(function (event) {
-  event.preventDefault();
+  /* event.preventDefault(); */
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+
   $("#triviaStartScreen").hide();
   $("#triviaContent").show();
-  setTimeout(function() {
-    alert("5 second countdown is up.")
-  }, 5000);
+  setInterval(function() { 
+    console.log("90 second countdown is up.")
+  }, 90000);
 
 });
 
+//decrement function here
+function decrement() {
+  timeLeft--;
+  $("#tickTock").html(timeLeft);
+  if (timeLeft === 0)
+  endTime();
+}
 
-//Trivia questions
+//end time function
+function endTime() {
+  clearInterval(intervalId);
+}
+
 
 
 
