ab7e105d67fbfe12e00e73756edd41a406234b84
Jason Kim <jasonkim4201@gmail.com>
2018-12-20 09:41:58 -0500

    added a basic html skeleton for question layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,27 @@
-$("#StartButton").click(function () {
-  $("#SplashScreen").hide();
-  $("#GameCanvas").show();
+
+//Add button to start the game
+//On clicking start button, a timer should start. setTimeout(functionToRun(), time in milliseconds)
+// Add a timer on screen reflecting the time left.  
+$(document).ready(function() {
+  console.log("ready");
 });
+
+//Total time
+var timeLeft = 120;
+
+
+$("#startButton").click(function (event) {
+  event.preventDefault();
+  $("#triviaStartScreen").hide();
+  $("#triviaContent").show();
+  setTimeout(function() {
+    alert("5 second countdown is up.")
+  }, 5000);
+
+});
+
+
+//Trivia questions
+
+
+
